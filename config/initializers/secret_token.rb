f5372b7e6dfc006a2e8e0619655354d5693fed32
Jack M <jackm@fundraise.com>
2013-11-02 00:08:31 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prescience::Application.config.secret_token = '04a7b836bfb0e46542918d4402972a4fc6c36f5e84004351dd632e723d4d7b20ce6835e1a1c3d86b4d63c1e0bf9768990e00d38f69cb1ca9e4dda4c3f79e6bc2'
